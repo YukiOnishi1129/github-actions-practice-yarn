24e36d29018cef02e6e5fb1ac806bac8a97863ef
yuki <yuki.onishi1129@gmail.com>
2021-05-16 16:27:29 +0900

    first commit

---
+++ __test__/index.test.js
@@ -0,0 +1,16 @@
+const fizzbuzz = require("../index");
+
+describe("fizzbuzz", () => {
+  it("return FizzBuzz when value is divisible by 15", () => {
+    expect(fizzbuzz(30)).toStrictEqual("FizzBuzz");
+  });
+  it("return FizzBuzz when value is divisible by 3", () => {
+    expect(fizzbuzz(9)).toStrictEqual("Fizz");
+  });
+  it("return FizzBuzz when value is divisible by 5", () => {
+    expect(fizzbuzz(10)).toStrictEqual("Buzz");
+  });
+  it("return FizzBuzz when value is divisible by 3 or 5", () => {
+    expect(fizzbuzz(7)).toStrictEqual("7");
+  });
+});
